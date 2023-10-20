1b989264cf4ad6bf7712f91d3760c684895629fa
Omnilon <97851047+Omnilon@users.noreply.github.com>
2023-10-20 18:53:46 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.post('/login', (req, res) => {
+  console.log('Login route hit, body:', req.body);
+  res.send('Simple login route is working!');
+});
