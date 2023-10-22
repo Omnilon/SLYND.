5e0e3c9b92a724b6624cf7ded109a576a03af96b
Omnilon <97851047+Omnilon@users.noreply.github.com>
2023-10-22 00:48:50 -0400

    revert cus chat is slow

--- public/scripts.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('show-register')?.addEventListener('click', function() {
-    document.getElementById('login-form').style.display = 'none';
-    document.getElementById('register-form').style.display = 'block';
-});
-
-document.getElementById('show-login')?.addEventListener('click', function() {
-    document.getElementById('register-form').style.display = 'none';
-    document.getElementById('login-form').style.display = 'block';
-});
