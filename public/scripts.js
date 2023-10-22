4f64588a98900bc64b03ef53d28175cfc2c6560b
Omnilon <97851047+Omnilon@users.noreply.github.com>
2023-10-22 00:38:59 -0400

    illousion styles.css
    this is when the scrpits.js file was added

---
+++ public/scripts.js
@@ -0,0 +1,9 @@
+document.getElementById('show-register')?.addEventListener('click', function() {
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('register-form').style.display = 'block';
+});
+
+document.getElementById('show-login')?.addEventListener('click', function() {
+    document.getElementById('register-form').style.display = 'none';
+    document.getElementById('login-form').style.display = 'block';
+});
