d53f9c1ed4ee5b36876962a183c15a372e5583a2
Omnilon <97851047+Omnilon@users.noreply.github.com>
2023-10-23 13:56:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ async function main() {
   app.set('view engine', 'ejs');
   app.set('views', path.join(__dirname, 'views'));
   app.use(express.static(path.join(__dirname, 'public')));
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.urlencoded({ extended: false }));
 
   const sessionStore = MongoStore.create({ client: client, dbName: 'SLYND', collectionName: 'sessions' });
   app.use(session({
