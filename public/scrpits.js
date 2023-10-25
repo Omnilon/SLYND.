0aef2be1a0b48833afcc99d82b49554c1fc020f5
Omnilon <97851047+Omnilon@users.noreply.github.com>
2023-10-24 20:11:54 -0400

    watev

--- public/scrpits.js
+++ public/scrpits.js
@@ -1,3 +1,4 @@
+//settings button
 document.getElementById('settings-button').addEventListener('click', function() {
     var console = document.getElementById('settings-console');
     if (console.classList.contains('hidden')) {
@@ -6,3 +7,14 @@ document.getElementById('settings-button').addEventListener('click', function() 
         console.classList.add('hidden');
     }
 });
+//logout button functionality 
+document.addEventListener('DOMContentLoaded', (event) => {
+    // This ensures that the DOM is fully loaded before attaching an event
+    var logoutButton = document.getElementById('logout-button');
+    if(logoutButton) { // Check if logoutButton exists to avoid null reference errors
+        logoutButton.addEventListener('click', function() {
+            document.getElementById('logout-form').submit();
+        });
+    }
+});
+//console .shrinking animation
